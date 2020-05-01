571e715520480938d45fa34201a2722983ee9b49
Jorge Ivan Guevara Perez <jorgei.guevarap@utadeo.edu.co>
2020-05-01 10:42:01 -0500

    script para instalar R y paquetes. Cambio de librerias Python

---
+++ R_dependencies.sh
@@ -0,0 +1,20 @@
+#!/bin/bash/
+
+echo "Insatall R"
+
+apt install dirmngr apt-transport-https ca-certificates software-properties-common gnupg2
+
+apt-key adv --keyserver keys.gnupg.net --recv-key 'E19F5F87128899B192B1A2C2AD5F960A256A04AF'
+
+add-apt-repository 'deb https://cloud.r-project.org/bin/linux/debian stretch-cran35/'
+
+
+apt update
+
+apt install r-base
+
+echo "END R Insatall"
+
+echo "Insatall Packages in R"
+install.packages("package")
+echo "End Packages Install"
